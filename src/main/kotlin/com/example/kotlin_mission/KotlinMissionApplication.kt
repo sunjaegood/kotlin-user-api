8f90fbe8b7b6e31f16cc95676696f20c5982d513
sunjaegood <sunjaegood@blockodyssey.io>
2025-08-07 23:52:53 +0900

    init: project setting

---
+++ src/main/kotlin/com/example/kotlin_mission/KotlinMissionApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_mission
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinMissionApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinMissionApplication>(*args)
+}
